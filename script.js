94802c0939993e157535b32e043ebca77aa03275
freshc0w <121275444+freshc0w@users.noreply.github.com>
2023-01-06 21:02:45 +1000

    remove game() and edit playRound() to stimulate games

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+const btns = document.querySelectorAll('button');
+btns.forEach(btn => btn.addEventListener('click', playRound))
+
 function getComputerChoice() {
     const options = ["rock", "paper", "scissors"]; 
     randomChoice = Math.floor(Math.random() * options.length) // Randomly returns a number in [0, 1, 2]
     return options[randomChoice]
 }
 
-function playRound(playerSelection, computerSelection=getComputerChoice()) {
-    playerSelection = playerSelection.toLowerCase();
+function playRound() {
+    let playerSelection = prompt('rock, paper or scissors?');
+    console.log(playerSelection);
+
+    computerSelection = getComputerChoice()
 
     let win = (playerSelection === "rock" && computerSelection === "scissors")
     || (playerSelection === "paper" && computerSelection === "rock")
@@ -14,44 +20,68 @@ function playRound(playerSelection, computerSelection=getComputerChoice()) {
     let lose = (computerSelection === "rock" && playerSelection === "scissors")
     || (computerSelection === "paper" && playerSelection=== "rock")
     ||(computerSelection === "scissors" && playerSelection === "paper")
-
+    
     if (playerSelection === computerSelection) {
-        return "It's a tie!";
+        return console.log("It's a tie!");
     } else if (win) {
-        return `You won! ${playerSelection} beats ${computerSelection}`; // Winning conditions satisfied
+        return console.log(`You won! ${playerSelection} beats ${computerSelection}`); // Winning conditions satisfied
     } else if (lose) {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        return console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
     } else {
-        playerSelection = prompt("Invalid move! Try again!");
-        return playRound(playerSelection);
+        console.log("Invalid Move! Try again!")
+        playRound()
     }
 }
-function game() {
-    let userScore = 0;
-    let botScore = 0;
-    for (let i=0; i < 5; i++) {
-        let playerSelection = prompt("Your move?", 'rock');
-        result = playRound(playerSelection);
 
-        outcome = result.split(' ');
-        if (outcome[1] === "won!") {
-            userScore += 1; 
-        } else if (outcome[1] === "lose!") {
-            botScore += 1;
-        } 
-        console.log(result)
-    }
+
+// function playRound(playerSelection="rock", computerSelection=getComputerChoice()) {
+//     // playerSelection = playerSelection.toLowerCase();
+
+//     let win = (playerSelection === "rock" && computerSelection === "scissors")
+//     || (playerSelection === "paper" && computerSelection === "rock")
+//     ||(playerSelection === "scissors" && computerSelection === "paper");
+
+//     let lose = (computerSelection === "rock" && playerSelection === "scissors")
+//     || (computerSelection === "paper" && playerSelection=== "rock")
+//     ||(computerSelection === "scissors" && playerSelection === "paper")
+
+//     if (playerSelection === computerSelection) {
+//         return "It's a tie!";
+//     } else if (win) {
+//         return `You won! ${playerSelection} beats ${computerSelection}`; // Winning conditions satisfied
+//     } else if (lose) {
+//         return `You lose! ${computerSelection} beats ${playerSelection}`;
+//     } else {
+//         console.log("Invalid Move! Try again!")
+//         playRound()
+
+// function game() {
+//     let userScore = 0;
+//     let botScore = 0;
+//     for (let i=0; i < 5; i++) {
+//         let playerSelection = prompt("Your move?", 'rock');
+//         result = playRound(playerSelection);
+
+//         outcome = result.split(' ');
+//         if (outcome[1] === "won!") {
+//             userScore += 1; 
+//         } else if (outcome[1] === "lose!") {
+//             botScore += 1;
+//         } 
+//         console.log(result)
+//     }
 
     
-    if (userScore > botScore) {
-        alert( "Congrats! You won against the bot!");
-    } else if (botScore > userScore) {
-        alert( "Oh no! You lost to a BOT!");
-    } else {
-        alert( "It's a tie!");
-    }
+//     if (userScore > botScore) {
+//         alert( "Congrats! You won against the bot!");
+//     } else if (botScore > userScore) {
+//         alert( "Oh no! You lost to a BOT!");
+//     } else {
+//         alert( "It's a tie!");
+//     }
 
-    finalOutcome = (`You won ${userScore}, lost ${botScore} and tied ${5 - (userScore + botScore)} times.`);
-    console.log(finalOutcome);
-    return finalOutcome;
-}
+//     finalOutcome = (`You won ${userScore}, lost ${botScore} and tied ${5 - (userScore + botScore)} times.`);
+//     console.log(finalOutcome);
+//     return finalOutcome;
+// }
+
